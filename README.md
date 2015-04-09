d133bea7d4c0feadf99f6dd869437aeaeff92eab
Lydia Stepanek <lydia.stepanek@gmail.com>
2015-04-08 20:40:27 -0400

    finish params.rb

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Rails Lite!
+# Rails Lite
 
 ## About
 
--- lib/phase5/params.rb
+++ lib/phase5/params.rb
@@ -10,9 +10,17 @@ module Phase5
     # You haven't done routing yet; but assume route params will be
     # passed in as a hash to `Params.new` as below:
     def initialize(req, route_params = {})
+      @params = {}
+      if req.query_string
+        @params.merge!(parse_www_encoded_form(req.query_string))
+      end
+      if req.body
+        @params.merge!(parse_www_encoded_form(req.body))
+      end
     end
 
     def [](key)
+      @params[key.to_s]
     end
 
     def to_s
@@ -22,17 +30,42 @@ module Phase5
     class AttributeNotFoundError < ArgumentError; end;
 
     private
+    # require 'uri'
+    # uri = URI("http://www.bing.com/search?q=x")
+    # URI::decode_www_form(uri.query)
+
     # this should return deeply nested hash
     # argument format
     # user[address][street]=main&user[address][zip]=89436
     # should return
     # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
     def parse_www_encoded_form(www_encoded_form)
+      params = Hash.new
+      if www_encoded_form
+        key_values = URI::decode_www_form(www_encoded_form)
+      #[["key", "val"], ["key2", "val2"]]
+        key_values.each do |arr|
+          nested_keys = parse_key(arr[0])
+          val = arr[1]
+          while nested_keys.length > 0
+            last_key = nested_keys.pop
+            temp_hash = Hash.new
+            temp_hash[last_key] = val
+            val = temp_hash
+          end
+          # best way to combine these 2 temp hashes?
+          # {"user"=>{"address"=>{"street"=>"main"}}}
+          # {"user"=>{"address"=>{"zip"=>"89436"}}}
+          params = params.deep_merge(temp_hash)
+        end
+      end
+      params
     end
 
     # this should return an array
     # user[address][street] should return ['user', 'address', 'street']
     def parse_key(key)
+      key.split(/\]\[|\[|\]/)
     end
   end
 end
